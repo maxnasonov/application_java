376eb91caeeabd5dfc26fa41f33dcec25f30be75
Jamie Ly <me@jamie.ly>
2014-02-27 09:47:24 -0500

    Added call to super in RemoteFile::Deploy constructor

--- libraries/provider_remote_file_deploy.rb
+++ libraries/provider_remote_file_deploy.rb
@@ -24,6 +24,8 @@ class Chef
       class Deploy < Chef::Provider::RemoteFile
 
         def initialize(new_resource, run_context)
+          super 
+          
           @deploy_resource = new_resource
           @new_resource = Chef::Resource::RemoteFile.new(@deploy_resource.name)
           @new_resource.path ::File.join(@deploy_resource.destination, ::File.basename(@deploy_resource.repository))
@@ -37,6 +39,8 @@ class Chef
           @current_resource = nil
           @run_context = run_context
           @converge_actions = nil
+          
+          @deployment_strategy ||= Chef::FileContentManagement::Deploy.strategy(true)
         end
 
         def target_revision
